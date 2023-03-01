f2f3f39701dcd3e11f8f2a602b05a7a1dd3eceea
Timothy Millward <timothymillward@Timothys-MacBook-Air.local>
2023-03-01 00:27:11 +0000

    test deploy

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBveyGgkvRe0Z3IW1TQUR3MDjUegVcw3uY",
-    authDomain: "next-door-nanny-83071.firebaseapp.com",
-    projectId: "next-door-nanny-83071",
-    storageBucket: "next-door-nanny-83071.appspot.com",
-    messagingSenderId: "589775122521",
-    appId: "1:589775122521:web:2924a089950f24e1408148",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
